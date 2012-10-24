112fcf9dcca11c8699db6d01a3a4e45f7ef48d8a
tatesuke <acousticdays@gmail.com>
2012-10-24 21:53:43 +0900

    trac用ご送信防止スクリプト

---
+++ operascript/submitPreventer.user.js
@@ -0,0 +1,57 @@
+(function() {
+
+	var ignoreValueList = [
+			"検索",
+			"複製",
+			"削除",
+			"ファイルを添付します",
+			"プレビュー",
+			"新しいマイルストーンの登録"];
+	var confirmMessage = "この操作はDBのデータを書き換える可能性があります。実行しますか?";
+
+	if (location.href.indexOf("example.com") != -1) {
+		preventSubmit("#FFFF00", true);
+	} else if(location.href.indexOf("localhost") != -1) {
+		preventSubmit("#00FF00", true);
+	}
+
+	function preventSubmit(buttonColor, isPrevent) {
+		var preventButtonList = [];
+
+		var ignoreValueMap = getIgnoreValueMap();
+		var inputElementList = document.getElementsByTagName("input");
+		for (var i = 0; i < inputElementList.length; i++) {
+			var inputElement = inputElementList[i];
+			var type = inputElement.getAttribute("type");
+			var value = inputElement.getAttribute("value");
+			if ((type == "submit") && !ignoreValueMap[value]) {
+				preventButtonList.push(inputElement);
+			}
+		}
+		
+		for (var i = 0; i < preventButtonList.length; i++) {
+			var preventButton = preventButtonList[i];
+			var value = preventButton.getAttribute("value");
+			preventButton.style.backgroundColor = buttonColor;
+			preventButton.setAttribute("value", value + "(！)");
+			if (isPrevent) {
+				preventButton.addEventListener("click", function(event){
+					var confirm = window.confirm(confirmMessage);
+					if (confirm == false) {
+						event.preventDefault();
+					}
+				}, true);
+			}
+		}
+	}
+	
+	function getIgnoreValueMap() {
+		var ignoreValueMap = {};
+		for (var i = 0; i < ignoreValueList.length; i++) {
+			var ignoreValue = ignoreValueList[i];
+			ignoreValueMap[ignoreValue] = true;
+		}
+		return ignoreValueMap;
+	}
+
+})();
